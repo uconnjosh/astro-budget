e0faac453568bdf2e8aca936a8c5d487664bbfb4
Josh Paul <softwarecreator@icloud.com>
2016-04-08 08:57:50 -0700

    add helper which returns a status message based on financial state of user

---
+++ app/helpers/financial-state.js
@@ -0,0 +1,14 @@
+import Ember from 'ember'
+
+export function financialState(params) {
+  let remainingFunds = params[0]
+
+  if (remainingFunds > 0) {
+    return 'money....'
+  } else {
+    return 'we are broke.......'
+  }
+
+}
+
+export default Ember.Helper.helper(financialState)
--- app/mirage/factories/budget.js
+++ app/mirage/factories/budget.js
@@ -6,7 +6,7 @@
 import Mirage/*, {faker} */ from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
- 'monthly-income': faker.list.random(3000, 2000, 5000),
+ 'monthly-income': faker.list.random(3000, 2000, 5000, 0),
  'monthly-saving': faker.list.random(0, 100, 200, 500, 1000, 1500, 1800),
  'monthly-debt':   faker.list.random(0, 100, 400, 500, 1000, 3500)
   // name: 'Pete',                         // strings
